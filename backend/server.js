7b294ddd147fadb89b03daaf17341503196c4f9d
Shivamsingh777 <shivamsingh271310@gmail.com>
2025-06-18 15:54:34 +0530

    commit

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import blogRouter from './routers/blog.route.js';
 import commentRouter from './routers/comment.route.js';
 import cors from 'cors'
 import cookieParser from "cookie-parser";
+import path from 'path'
 
 dotenv.config()
 const app = express();
@@ -13,19 +14,18 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended:true}))
 app.use(cors({
-    origin: ["http://localhost:5173","https://astounding-sawine-26145b.netlify.app/"],
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
+const _dirname = path.resolve()
 app.use("/api", userRouter);
 app.use("/api/v1/blog", blogRouter);
 app.use("/api/v1/comment", commentRouter);
-app.get('/',(req,res)=>{
-    res.send({
-        activeStatus:true,
-        error: false,
-    })
-})
+app.use(express.static(path.join(_dirname, "frontend/dist")))
+// app.get("*", (_, res)=>{
+//     res.sendFile(path.resolve(_dirname, "frontend", "dist", "intex.html"))
+// })
 const PORT = process.env.PORT || 5000;
 connectToMongoDB()
 app.listen(PORT, () => { 
